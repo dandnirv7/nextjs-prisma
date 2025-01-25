ef41da4102bc3a8500d398c9fc0dae997c177b26
dandnirv7 <dandnirv7@gmail.com>
2025-01-25 23:44:56 +0700

    feat: implement password reset functionality with email notification and token management

--- src/app/utils/errorMessage.ts
+++ src/app/utils/errorMessage.ts
@@ -3,10 +3,11 @@ export const ERROR_MESSAGES = {
   DELETE_FAILED: "Failed to delete user.",
   INTERNAL_SERVER_ERROR: "Internal server error",
   INVALID_CREDENTIALS: "Invalid credentials",
+  INVALID_EMAIL: "Invalid email address",
   INVALID_ID: "Invalid ID format.",
   LOGIN_FAILED: "Invalid login attempt. Username or password is incorrect.",
   LOGOUT_INTERNAL_ERROR: "Failed to logout, internal server error.",
-  METHOD_NOT_ALLOWED: "Method not allowed for logout.",
+  METHOD_NOT_ALLOWED: "Method not allowed.",
   PROCESS_FAILED: "Failed to process request.",
   REGISTER_FAILED:
     "Registration failed. Username or email might already be in use.",
